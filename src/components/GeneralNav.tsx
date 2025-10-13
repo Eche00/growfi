8a587c72a27ad6cb38b581f575d41db91f7559e9
Eche00 <echeeze956@gmail.com>
2025-10-13 23:31:24 +0100

    mobile nav bg color

--- src/components/GeneralNav.tsx
+++ src/components/GeneralNav.tsx
@@ -10,20 +10,19 @@ function GeneralNav({setDropDown}: DropdownProp) {
     <div onClick={()=>setDropDown(false)}>
       <section 
       
-       className="fixed bg-white top-[90px] left-0 w-full h-[100vh]  z-50 flex flex-col gap-[32px] md:hidden">
+       className="fixed bg-gradient-to-b from-black via-[#7E7ED7]  to-black top-[90px] left-0 w-full h-[100vh]  z-50 flex flex-col gap-[32px] md:hidden">
           <div className="flex flex-col  text-white text-[16px] font-[500]" 
           
           >
-          <NavLink to='/' className={({isActive})=> isActive ? 'text-green-700 font-bold text-sm cursor-pointer p-[10px]  border-b border-gray-500': ' text-gray-600 font-bold text-sm cursor-pointer p-[10px]  border-b border-green-700'}>Home</NavLink>
-          <NavLink to='/mentorship' className={({isActive})=> isActive ? 'text-green-700 font-bold text-sm cursor-pointer p-[10px]  border-b border-green-700': ' text-gray-600 font-bold text-sm cursor-pointer p-[10px]  border-b border-green-700'}>Mentorship</NavLink>
-          <NavLink to='/signals' className={({isActive})=> isActive ? 'text-green-700 font-bold text-sm cursor-pointer p-[10px]  border-b border-green-700': ' text-gray-600 font-bold text-sm cursor-pointer p-[10px]  border-b border-green-700'}>Signals</NavLink>
-          <NavLink to='/courses' className={({isActive})=> isActive ? 'text-green-700 font-bold text-sm cursor-pointer p-[10px]  border-b border-green-700': ' text-gray-600 font-bold text-sm cursor-pointer p-[10px]  border-b border-green-700'}>Courses </NavLink>
+          <NavLink to='/' className={({isActive})=> isActive ? 'text-[#68FCC6] font-bold text-sm cursor-pointer p-[10px]  border-b border-gray-500': ' text-gray-300 font-bold text-sm cursor-pointer p-[10px]  border-b border-[#68FCC6]'}>Home</NavLink>
+          <NavLink to='/mentorship' className={({isActive})=> isActive ? 'text-[#68FCC6] font-bold text-sm cursor-pointer p-[10px]  border-b border-[#68FCC6]': ' text-gray-300 font-bold text-sm cursor-pointer p-[10px]  border-b border-[#68FCC6]'}>Mentorship</NavLink>
+          <NavLink to='/signals' className={({isActive})=> isActive ? 'text-[#68FCC6] font-bold text-sm cursor-pointer p-[10px]  border-b border-[#68FCC6]': ' text-gray-300 font-bold text-sm cursor-pointer p-[10px]  border-b border-[#68FCC6]'}>Signals</NavLink>
+          <NavLink to='/courses' className={({isActive})=> isActive ? 'text-[#68FCC6] font-bold text-sm cursor-pointer p-[10px]  border-b border-[#68FCC6]': ' text-gray-300 font-bold text-sm cursor-pointer p-[10px]  border-b border-[#68FCC6]'}>Courses </NavLink>
            </div>
             
           <div className="flex sm:flex-row flex-col sm:items-center gap-[20px]">
              
-           <Link to='/calculator'  className=" px-[32px] py-[10px] text-[16px] mx-[20px] rounded-[8px] bg-green-700 text-white cursor-pointer hover:scale-[105%] transition-all duration-300 text-center flex-1">Risk Calculate</Link>
-
+          <Link to='/calculator'  className="py-[8px] px-[32px] w-fit  bg-black text-white flex  items-center justify-center rounded-full shadow-[0_0_6px_0_#68FCC6] ml-auto mr-10">Get started</Link>
           </div>
 
          
